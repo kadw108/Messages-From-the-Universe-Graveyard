a05982b649d3ee74b1eed611a561bb55221ecc32
kadw108 <wallwritinglast@gmail.com>
2024-01-07 00:08:32 -0500

    Add node number cross-references, correct node IDs

--- script_files/coordinateMap.ts
+++ script_files/coordinateMap.ts
@@ -1,17 +1,17 @@
 export const coordinateMap = {
-    108: "skyGrove",
-    8275: "greenhouseWithinFlower",
-    71: "drownedApparatus",
-    38539: "nexusDome",
+    81818181: "skyGrove",
+    19283: "greenhouseWithinFlower",
+    8108: "drownedApparatus",
+    1911: "nexusDome",
     9272: "maintenanceTunnelXEnd",
-    91374: "caterpillarsStomach",
+    67: "caterpillarsStomach",
 
-    823: "familiarPlaceApparatus",
-    88888: "bedroom0",
+    2024: "familiarPlaceApparatus",
+    416: "bedroom0",
     12: "bridgeSkyIsland",
-    362: "dungeonCenter",
-    121212: "hiddenGrove",
-    666: "obsidianGrotto",
-    2022: "transmissionSummitBase",
-    24815: "cellEast"
+    810: "dungeonCenter",
+    152: "hiddenGrove",
+    666: "goldGrotto",
+    340: "transmissionSummitBase",
+    124: "cellEast"
 };
