345e833db6356411c1fd73a55273859d091bfeab
kadw108 <wallwritinglast@gmail.com>
2024-01-31 23:22:12 -0500

    v1.1.2 rework About page, change version

--- script_files/addReplaceLink.ts
+++ script_files/addReplaceLink.ts
@@ -1,10 +1,15 @@
 /*
 Syntax:
 
-<a class="linkReplaced" identifier="test" >How awful.</a>
-<p class="removedOnClick" identifier="test" >It sucks.</p>
-<span class="linkReplacer hidden" identifier="test">How wonderful.</span>
+<a class="linkReplaced" identifier="test" >Shows message on click.</a>
+<a class="linkHider" identifier="test" >Hides message on click.</a>
+<p class="removedOnClick" identifier="test" >Disappears when message is shown.</p>
+<span class="linkReplacer hidden" identifier="test">Hidden text.</span>
 
+<a class="linkReplaced removedOnClick" identifier="test" >Shows message on click.</a>
+<span class="linkReplacer hidden" identifier="test"><a class="linkHider" identifier="test" >Hides message on click.</a>
+
+Hidden text.</span>
 */
 
 function clickReplaceLink(event) {
@@ -17,6 +22,9 @@ function clickReplaceLink(event) {
         return;
     }
     replacers.forEach(r => {
+        if (!r.classList.contains("hidden")) {
+            return;
+        }
         r.classList.remove("hidden");
     });
 
@@ -26,8 +34,34 @@ function clickReplaceLink(event) {
     });
 }
 
+function clickHideText(event) {
+    const identifier = event.target.getAttribute("identifier");
+
+    const replacers = document.querySelectorAll(".linkReplacer[identifier='" + identifier + "']");
+    if (replacers.length === 0) {
+        console.error("Replacelink without replacer! Identifier " + identifier);
+        console.error(event.target);
+        return;
+    }
+    replacers.forEach(r => {
+        if (r.classList.contains("hidden")) {
+            return;
+        }
+        r.classList.add("hidden");
+    });
+
+    const removed = document.querySelectorAll(".removedOnClick[identifier='" + identifier + "']");
+    removed.forEach(r => {
+        r.classList.remove("hidden");
+    });
+}
+
 export function addReplaceLink() {
     Array.from(document.getElementsByClassName("linkReplaced")).forEach(e => {
         e.addEventListener("click", clickReplaceLink);
+    });
+
+    Array.from(document.getElementsByClassName("linkHider")).forEach(e => {
+        e.addEventListener("click", clickHideText);
     });
 }
