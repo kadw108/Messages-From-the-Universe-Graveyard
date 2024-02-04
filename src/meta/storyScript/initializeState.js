7842a3a7c7af727b7d3fb7e9cfc69f2e7f928a8e
kadw108 <wallwritinglast@gmail.com>
2024-02-04 04:45:25 -0500

    v1.1.3 Reduce asset sizes, tweak CSS

--- src/meta/storyScript/initializeState.js
+++ src/meta/storyScript/initializeState.js
@@ -1,2 +1,17 @@
 story.transitionMessage = null;
 s.visitedSphere = 0;
+
+/* To preload images */
+/* From https://twinery.org/forum/discussion/8195/preloading-background-images-sugarcube-2-0-twine-2 */
+(function () {
+	const preload = [
+        "assets/loading.gif",
+        "assets/1dorm.png.gif",
+    ];
+
+	window._ImageCache = preload.map(function (url) {
+		const image = document.createElement('img');
+		image.src = url;
+		return image;
+	});
+})();
