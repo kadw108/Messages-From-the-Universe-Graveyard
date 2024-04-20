b3a09fcd3dff6c514eedd28dc0b921963bfdba57
kadw108 <wallwritinglast@gmail.com>
2024-04-20 04:12:16 -0400

    v1.1.6 CSS/writing tweaks, bugfix
    Fix bug where black screen persisted when using certain
    menu panels to enter other rooms, leading to a state
    where nothing can be clicked

--- script_files/addMenuPanels.ts
+++ script_files/addMenuPanels.ts
@@ -2,7 +2,10 @@
 Syntax:
 
 <a class="panelOpener" identifier="test" >Click to make it show up!</a>
-<div class="menuPanel absoluteAlign hidden" identifier="test">How wonderful. I love my life.</div>
+<div class="menuPanel absoluteAlign hidden" identifier="test">
+    How wonderful. I love my life.
+    <a class="panelCloser" identifier="test">Click to close panel.</a>
+</div>
 
 */
 
@@ -26,22 +29,25 @@ function openMenuPanel(event) {
     bg.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
+function closeMenuPanelHandler(menuPanel) {
+    return () => {
+        menuPanel.classList.add("hidden");
+
+        const bg = document.getElementById("screenCover");
+        if (bg === null) {
+            return;
+        }
+        bg.style.zIndex = "-1";
+    }
+}
+
 function editMenuPanel(menuPanel) {
     if (menuPanel.getAttribute("hasCloseButton") !== "true") {
         const closeButton = document.createElement("button");
         closeButton.innerText = "X";
         closeButton.type = "button";
         closeButton.classList.add("closeButton");
-        closeButton.addEventListener("click", () => {
-            menuPanel.classList.add("hidden");
-
-            const bg = document.getElementById("screenCover");
-            if (bg === null) {
-                return;
-            }
-            bg.style.zIndex = "-1";
-        });
-
+        closeButton.addEventListener("click", closeMenuPanelHandler(menuPanel));
         menuPanel.prepend(closeButton);
 
         menuPanel.setAttribute("hasCloseButton", "true");
@@ -55,5 +61,11 @@ export function addMenuPanels() {
 
     Array.from(document.getElementsByClassName("menuPanel")).forEach(e => {
         editMenuPanel(e);
+    });
+
+    Array.from(document.getElementsByClassName("panelCloser")).forEach(e => {
+        const identifier = e.getAttribute("identifier");
+        const panelToClose = document.querySelector(".menuPanel[identifier='" + identifier + "']");
+        e.addEventListener("click", closeMenuPanelHandler(panelToClose));
     });
 }
